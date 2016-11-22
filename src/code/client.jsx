116659795adc28c45ed5929c065be7de58840dc1
Kevin Ghadyani <Saturn2888@gmail.com>
2016-11-21 21:37:25 -0600

    Cleaned up client.jsx

--- src/code/client.jsx
+++ src/code/client.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
-// import { match } from 'react-router'
 
 // Root Component
 import Root from 'root'
@@ -9,28 +8,19 @@ import Root from 'root'
 import 'react-fastclick'
 import 'utilities/polyfills'
 
-// Store and Routes
-// import { history } from 'utilities/store'
-// import routes from './routes'
-
 // Router
 const RootElement = document.getElementById('root')
-// match({ history, routes }, (error, redirectLocation, renderProps) => {
-	// if (redirectLocation) {
-	// 	window.location = redirectLocation.pathname
-	// }
 
-	render(
-		<Root />
-	, RootElement)
+render(
+	<Root />
+, RootElement)
 
-	if (module.hot) {
-		module.hot.accept('./root', () => {
-			const RootContainer = require('./root').default
+if (module.hot) {
+	module.hot.accept('./root', () => {
+		const RootContainer = require('./root').default
 
-			render(
-				<RootContainer />
-			, RootElement)
-		})
-	}
-// })
+		render(
+			<RootContainer />
+		, RootElement)
+	})
+}
