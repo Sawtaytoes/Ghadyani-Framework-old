271e034703b3fbac9f140e57cfddc25d2c5d335a
Kevin Ghadyani <Saturn2888@gmail.com>
2016-11-21 20:17:42 -0600

    Updated Root to function w/ client-side async routes

--- src/code/root.jsx
+++ src/code/root.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Match, Miss } from 'react-router'
@@ -6,26 +6,38 @@ import { BrowserRouter as Router, Match, Miss } from 'react-router'
 // Store and Routes
 import { store } from 'utilities/store'
 
-// Views
-// import Home from 'views/home'
+export default class Root extends PureComponent {
+	constructor() {
+		super()
 
-// console.log(require('./views/home'));
+		this.views = {}
+		this.viewReady = new Set()
 
-const Home = () => {
-	const home = require('./views/home')
-	return (<div>hi <home /></div>)
-}
-// const Home = Promise.resolve(require('./views/home'))
-// const Home = () => require.ensure([], (require) => require('./views/home'))
+		this.loadView = fileName => {
+			if (this.viewReady.has(fileName)) {
+				this.viewReady.delete(fileName)
+				return this.views[fileName]
+			}
 
-export default class Root extends Component {
+			new Promise(resolve => require.ensure([], () => {
+				resolve(require(`./views/${fileName}`))
+			}))
+			.then(View => this.views[fileName] = <View />)
+			.then(() => this.viewReady.add(fileName))
+			.then(() => this.forceUpdate())
+
+			return <div />
+		}
+	}
+
 	render() { return (
 		<AppContainer>
 			<Provider store={store}>
 				<Router>
 					<div>
-						<Match exactly pattern="/" render={Home} />
-						{/*<Miss component="">*/}
+						<Match exactly pattern="/" component={this.loadView.bind(this, 'home')} />
+						<Match exactly pattern="/about" component={this.loadView.bind(this, 'about')} />
+						<Miss component={this.loadView.bind(this, '404')} />
 					</div>
 				</Router>
 			</Provider>
