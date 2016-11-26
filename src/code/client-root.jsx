c30963aa6d30874f6778a62d79444be0b2d45afa
Kevin Ghadyani <Saturn2888@gmail.com>
2016-11-25 19:09:54 -0600

    Async routes now work w/ React Hot Loader by making ClientRoot not Pure

--- src/code/client-root.jsx
+++ src/code/client-root.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
 import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router'
@@ -9,7 +9,7 @@ import { store } from 'utilities/store'
 // Components
 import Routes from 'routes'
 
-export default class ClientRoot extends PureComponent {
+export default class ClientRoot extends Component {
 	render() { return (
 		<AppContainer>
 			<Provider store={store}>
