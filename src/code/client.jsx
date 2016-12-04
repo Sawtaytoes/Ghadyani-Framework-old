93ee5f1a47658149ae0daef5943b76d59209aebe
Kevin Ghadyani <Saturn2888@gmail.com>
2016-12-03 22:03:20 -0600

    Failed attempt at implementing inferno-router

--- src/code/client.jsx
+++ src/code/client.jsx
@@ -1,17 +1,17 @@
 import Inferno, { render } from 'inferno'
-// import { Router, Route, browserHistory } from 'inferno-router'
+import { Router, Route, browserHistory } from 'inferno-router'
 
 // Root Component
 // import ClientRoot from 'client-root'
 
 // Vars
 const RootElement = document.getElementById('root')
-render(<div>hi</div>, RootElement)
-// render(
-// 	<Router history={browserHistory}>
-// 		<Route path="*" component={<div>hi</div>} />
-// 	</Router>
-// , RootElement)
+// render(<div>hi</div>, RootElement)
+render(
+	<Router history={browserHistory}>
+		<Route path="*" component={<div>hi</div>} />
+	</Router>
+, RootElement)
 
 // render(
 // 	<ClientRoot />
