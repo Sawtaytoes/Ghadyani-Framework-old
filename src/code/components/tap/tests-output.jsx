73634a22226a80e399c25044694d42d02344f4ab
Kevin Ghadyani <Saturn2888@gmail.com>
2016-12-15 01:11:09 -0600

    Removed unused murmurhash from TestsOutput

--- src/code/components/tap/tests-output.jsx
+++ src/code/components/tap/tests-output.jsx
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
-import hash from 'murmurhash-js'
 
 // Components
 import TestsStats from 'components/tap/tests-stats'
