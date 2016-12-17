656df6f1b31b1c54afdaefe57b3f3984c1d76266
Kevin Ghadyani <Saturn2888@gmail.com>
2016-12-17 14:49:43 -0600

    Tweaked Webpack Dev Server config settings for https to match docs

--- webpack/webpack-dev-server.config.js
+++ webpack/webpack-dev-server.config.js
@@ -8,13 +8,13 @@ const paths = require(`${dir.includes}paths`)
 const webpackClientConfig = require(`${dir.webpack}webpack.config.client.dev`)
 
 module.exports = {
-	https: config.isSecure(),
-	cert: config.isSecure() ? fs.readFileSync('./conf/domain-crt.txt') : '',
-	key: config.isSecure() ? fs.readFileSync('./conf/key.pem') : '',
-
 	contentBase: `${global.baseDir}${paths.root.dest}`,
 	historyApiFallback: true,
 	hot: true,
+	https: config.isSecure() && {
+		cert: config.isSecure() ? fs.readFileSync('./conf/domain-crt.txt') : '',
+		key: config.isSecure() ? fs.readFileSync('./conf/key.pem') : '',
+	},
 	// progress: true,
 	proxy: {
 		'*': { target: config.getProxyServerUrl() }
