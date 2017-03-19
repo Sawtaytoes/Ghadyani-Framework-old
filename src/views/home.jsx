201803f20a64645a520073d7c0469f2687ba8ee4
Kevin Ghadyani <Saturn2888@gmail.com>
2017-03-19 00:42:59 -0500

    Added Sample to Home

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,6 +1,9 @@
 import React, { PureComponent } from 'react'
 import { Link } from 'react-router-dom'
 
+// Components
+import Sample from 'components/sample'
+
 // Utilities
 import StylesLoader from 'utilities/styles-loader'
 
@@ -12,9 +15,12 @@ class Home extends PureComponent {
 		<div>
 			<h1>Hello World</h1>
 			<Link to="/about" title="Go to About">About</Link>
+
 			<p>
 				Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
 			</p>
+
+			<Sample />
 		</div>
 	)}
 }
