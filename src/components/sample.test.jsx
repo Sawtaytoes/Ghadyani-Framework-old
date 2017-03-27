fd3a1ae9da6c45bb83fefd3f3492bac1dc5cc43d
Kevin Ghadyani <Saturn2888@gmail.com>
2017-03-26 20:24:47 -0500

    Minor code cleanup in Sample

--- src/components/sample.test.jsx
+++ src/components/sample.test.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 import ReactTestUtils from 'react-addons-test-utils'
 import test from 'tape-catch'
 
-// Utilities
 import TestHelper from 'utils/test-helper'
-
-// Components
 import Sample from 'components/sample'
 
 class TestRun extends TestHelper {
@@ -27,7 +24,6 @@ class TestRun extends TestHelper {
 
 test('Render <Sample />', t => {
 	const testRun = new TestRun(t)
-	// const options = TestRun.getVars()
 
 	const component = testRun.renderSample()
 	t.ok(component, "<Sample /> component should exist")
