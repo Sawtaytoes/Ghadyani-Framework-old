c3841628b11dcc28267f54da38295fc31bb85e50
Kevin Ghadyani <Saturn2888@gmail.com>
2017-03-29 23:34:28 -0500

    Removed tap from global ducks as it's not used
    The tap reducer was in the global site ducks, but it's pulled in specifically for the tests entry point and is unused in the main site.

--- src/ducks/index.jsx
+++ src/ducks/index.jsx
@@ -3,10 +3,8 @@ import { routerReducer as routing } from 'react-router-redux'
 
 // Reducers
 import location from 'ducks/location'
-import tap from 'ducks/tap'
 
 export default combineReducers({
 	location,
-	tap,
 	routing,
 })
