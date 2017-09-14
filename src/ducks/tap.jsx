965b3d59e6ef1652824681df860005691ec915c6
Kevin Ghadyani <Saturn2888@gmail.com>
2017-09-13 23:20:50 -0500

    Updated most of TAP parser reducer to be functional
    Everything is now functional apart from the failure messages parser.

--- src/ducks/tap.jsx
+++ src/ducks/tap.jsx
@@ -38,16 +38,6 @@ export const TAP_MESSAGE_REGEX = /^((ok|not ok|(# (ok|tests|pass|fail)?))[ ]*)(.
 export const TAP_TEST_INFO_REGEX = /^(\d+)[ ](.+)$/
 export const TAP_FAILURE_REGEX = /^((\s{4}(operator|expected|actual|stack):)|\s{6})[ ]*(.+)$/
 
-export const getInitialState = () => ({
-	duration: 0,
-	failures: [],
-	numFailed: 0,
-	numPassed: 0,
-	numTotal: 0,
-	tests: [],
-	testsComplete: false,
-})
-
 const Enum = () => ({})
 export const TAP_MESSAGE_TYPE = {
 	HEADER: Enum(),
@@ -61,6 +51,17 @@ export const TAP_COLOR = {
 	PASS: 'green',
 }
 
+const getFailureInfo = message => message.match(TAP_FAILURE_REGEX)
+
+const getMessageInfo = message => {
+	const [ , , identifier, , , messageText] = message.match(TAP_MESSAGE_REGEX) || []
+
+	return {
+		identifier,
+		messageText,
+	}
+}
+
 const getTestInfo = string => {
 	const [_, testNumber, text] = string.match(TAP_TEST_INFO_REGEX)
 
@@ -70,106 +71,144 @@ const getTestInfo = string => {
 	}
 }
 
+const isSuccessfulEndMessage = message => message === '# ok'
+
+const messageMatchesIdentifier = (
+	identifier => ({ messageIdentifier }) => (
+		identifier === messageIdentifier
+	)
+)
+
+const tapCountActions = {
+	'# fail': count => ({
+		numFailed: Number(count),
+		testsComplete: true,
+	}),
+
+	'# ok': () => ({
+		testsComplete: true,
+	}),
+
+	'# pass': count => ({
+		numPassed: Number(count),
+	}),
+
+	'# tests': count => ({
+		numTotal: Number(count),
+	}),
+}
+
+const tapTestActions = {
+	'# ': ({ tests }, text) => ({
+		tests: (
+			tests
+			.concat({
+				text,
+				type: TAP_MESSAGE_TYPE.HEADER,
+			})
+		)
+	}),
+
+	'not ok': ({ numFailed, numTotal, tests }, messageText) => ({
+		numFailed: numFailed + 1,
+		numTotal: numTotal + 1,
+		tests: (
+			tests
+			.concat({
+				...getTestInfo(messageText),
+				type: TAP_MESSAGE_TYPE.FAIL,
+			})
+		),
+	}),
+
+	'ok': ({ numPassed, numTotal, tests }, messageText) => ({
+		numPassed: numPassed + 1,
+		numTotal: numTotal + 1,
+		tests: (
+			tests
+			.concat({
+				...getTestInfo(messageText),
+				type: TAP_MESSAGE_TYPE.PASS,
+			})
+		),
+	}),
+}
+
+const getParseActions = parseActions => (
+	Object
+	.keys(parseActions)
+	.map(
+		messageIdentifier => ({
+			messageIdentifier,
+			parseAction: parseActions[messageIdentifier]
+		})
+	)
+)
+
+const tapCountParseActions = getParseActions(tapCountActions)
+const tapTestParseActions = getParseActions(tapTestActions)
+
 
 // --------------------------------------------------------
 // Reducer
 // --------------------------------------------------------
 
-export default (state = getInitialState(), action) => {
-	const {
-		type,
-		startTime,
-		message,
-	} = action
+export const getInitialState = () => ({
+	duration: 0,
+	failures: [],
+	numFailed: 0,
+	numPassed: 0,
+	numTotal: 0,
+	tests: [],
+	testsComplete: false,
+})
 
-	switch (type) {
-	case SET_TAP_START_TIME:
-		return {
+const reducer = {
+	[SET_TAP_START_TIME]: (
+		(state, { startTime }) => ({
 			...state,
-			startTime
-		}
+			startTime,
+		})
+	),
 
-	case ADD_TAP_MESSAGE: {
-		const newState = { ...state }
+	[ADD_TAP_MESSAGE]: (
+		(state, { message }) => {
+			const { startTime } = state
+			const endTime = new Date()
+			const duration = (endTime - startTime) / SECOND_IN_MILLISECONDS
 
-		newState.endTime = new Date()
+			const { identifier, messageText } = getMessageInfo(message)
 
-		newState.duration = (
-			(newState.endTime - newState.startTime) / SECOND_IN_MILLISECONDS
-		)
-
-		const test = {}
-		const value = (message.match(TAP_MESSAGE_REGEX) || [])[5]
-
-		const tapMessageActions = {
-			'# fail': () => {
-				newState.numFailed = Number(value)
-				newState.testsComplete = true
-			},
-			'# ok': () => {
-				newState.testsComplete = true
-			},
-			'# pass': () => {
-				newState.numPassed = Number(value)
-			},
-			'# tests': () => {
-				newState.numTotal = Number(value)
-			},
-			'# ': () => {
-				test.type = TAP_MESSAGE_TYPE.HEADER
-				test.text = value
-
-				newState.tests = state.tests.slice()
-				newState.tests.push(test)
-			},
-			'not ok': () => {
-				const { testNumber, text } = getTestInfo(value)
-
-				test.type = TAP_MESSAGE_TYPE.FAIL
-				test.text = text
-				test.testNumber = testNumber
-
-				newState.tests = state.tests.slice()
-				newState.tests.push(test)
-
-				newState.numTotal += 1
-				newState.numFailed += 1
-			},
-			'ok': () => {
-				const { testNumber, text } = getTestInfo(value)
-
-				test.type = TAP_MESSAGE_TYPE.PASS
-				test.text = text
-				test.testNumber = testNumber
-
-				newState.tests = state.tests.slice()
-				newState.tests.push(test)
-
-				newState.numTotal += 1
-				newState.numPassed += 1
-			},
-		}
-
-		const getNewState = (
-			Object
-			.keys(tapMessageActions)
-			.map(
-				tapMessageIdentifier => ({
-					tapMessageIdentifier,
-					parseAction: tapMessageActions[tapMessageIdentifier]
-				})
+			const [tapCounts] = (
+				tapCountParseActions
+				.filter(messageMatchesIdentifier(identifier))
+				.map(({ parseAction }) => parseAction(messageText))
 			)
-			.find(({ tapMessageIdentifier }) => message.startsWith(tapMessageIdentifier))
-		)
 
-		getNewState && getNewState.parseAction()
+			const [tapTests] = (
+				tapTestParseActions
+				.filter(messageMatchesIdentifier(identifier))
+				.map(({ parseAction }) => parseAction(state, messageText))
+			)
 
-		return { ...newState }
-	}
+			const successfulEndState = (
+				isSuccessfulEndMessage(message)
+				&& tapCountActions[message]
+			)
 
-	case ADD_TAP_FAILURE: {
+			return {
+				...state,
+				...tapCounts,
+				...(successfulEndState || tapTests),
+				duration,
+				endTime,
+			}
+		}
+	),
+
+	[ADD_TAP_FAILURE]: (state, { message }) => {
 		const newState = { ...state }
-		const failureInfo = message.match(TAP_FAILURE_REGEX)
+		const failureInfo = getFailureInfo(message)
 
 		const failureType = failureInfo[3]
 		const failureReason = failureInfo[4]
@@ -204,9 +243,13 @@ export default (state = getInitialState(), action) => {
 		}
 
 		return { ...newState }
-	}
-
-	default:
-		return state
-	}
+	},
 }
+
+const fpReducer = (reducer, initialState) => (state = initialState, action) => (
+	reducer[action.type]
+	? reducer[action.type](state, action)
+	: state
+)
+
+export default fpReducer(reducer, getInitialState())
