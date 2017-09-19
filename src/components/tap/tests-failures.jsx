d62472f56cfa299749cbf53a3c293234fe89ae2c
Kevin Ghadyani <Saturn2888@gmail.com>
2017-09-19 17:52:58 -0500

    Removed unnecessarys semi-colons from React imports

--- src/components/tap/tests-failures.jsx
+++ src/components/tap/tests-failures.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import React from 'react';
+import React from 'react'
 import { connect } from 'react-redux'
 
 import TestFailure from 'components/tap/test-failure'
