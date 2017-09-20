cb7339f3ac7acf0e4b395b29772c672b821028bd
Kevin Ghadyani <Saturn2888@gmail.com>
2017-09-20 04:18:00 -0500

    Added placeholder for pageMeta tests

---
+++ src/ducks/pageMeta.test.js
@@ -0,0 +1,12 @@
+import test from 'tape-catch'
+
+// import pageMetaReducer, {
+// 	changeLocation,
+// 	updatePageMeta,
+
+// 	initialState,
+// } from './pageMeta'
+
+test('Page Meta: Test', t => {
+	t.ok(true, 'yo!')
+})
