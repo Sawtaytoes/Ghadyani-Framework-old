26a1bc78328e48c47155a3a2c6826c817069347a
Kevin Ghadyani <Saturn2888@gmail.com>
2017-09-20 20:52:48 -0500

    Added ability to run tests based on a wildcard
    Run `/tests/tap*` to pull in all tests in a `tap/` directory.

--- src/tests.js
+++ src/tests.js
@@ -9,6 +9,7 @@ import store from 'utils/testsStore'
 // Setup testing files to watch
 const context = require.context('./', true, /^\.\/.*\.test\.js$/)
 const testName = window.__TESTNAME__
+const isWildcard = testName.match(/\*$/)
 
 const runTests = isTestFile => (
 	context.keys()
@@ -17,8 +18,15 @@ const runTests = isTestFile => (
 )
 
 const allTests = fileName => !fileName.includes('TapOutput.test.js')
+
 const specificTests = testName => fileName => (
-	fileName.includes(`/${testName}.test.js`)
+	isWildcard && !fileName.includes('TapOutput.test.js')
+	? (
+		fileName.match(
+			new RegExp(`${testName.replace('*', '')}.*\\.test\\.js`)
+		)
+	)
+	: fileName.includes(`/${testName}.test.js`)
 )
 
 runTests(
