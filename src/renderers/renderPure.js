da4fb74189b38a68ae10c5b1086c1faa5cbc03ad
Kevin Ghadyani <Saturn2888@gmail.com>
2017-09-26 00:48:45 -0500

    Updated renderPure shouldComponentUpdate to not always cause rerenders

--- src/renderers/renderPure.js
+++ src/renderers/renderPure.js
@@ -3,7 +3,14 @@ import React, { PureComponent } from 'react'
 const renderPure = ComposedComponent => (
 	class Pure extends PureComponent {
 		shouldComponentUpdate(props, nextProps) {
-			return props !== nextProps
+			return (
+				Object.keys(props)
+				.every(key => props[key] === nextProps[key])
+				&& (
+					Object.keys(nextProps)
+					.every(key => nextProps[key] === props[key])
+				)
+			)
 		}
 
 		render() {
