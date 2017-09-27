fddea68c1580d302d690c60705e5415621888dfa
Kevin Ghadyani <Saturn2888@gmail.com>
2017-09-26 22:05:35 -0500

    Fixed a bug with renderPure() wrapper

--- src/renderers/renderPure.js
+++ src/renderers/renderPure.js
@@ -2,17 +2,6 @@ import React, { PureComponent } from 'react'
 
 const renderPure = ComposedComponent => (
 	class Pure extends PureComponent {
-		shouldComponentUpdate(props, nextProps) {
-			return (
-				Object.keys(props)
-				.every(key => props[key] === nextProps[key])
-				&& (
-					Object.keys(nextProps)
-					.every(key => nextProps[key] === props[key])
-				)
-			)
-		}
-
 		render() {
 			return <ComposedComponent {...this.props} />
 		}
