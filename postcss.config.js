5764458f1cb2a1dbd80318c9024c3da95efdffef
Kevin Ghadyani <Saturn2888@gmail.com>
2017-09-28 19:39:08 -0500

    Updated formatting of postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
-module.exports = { plugins: [
-	require('autoprefixer')({ browsers: [
-		'last 4 versions',
-		'> 5%',
-		'ie 11',
-		'not ie <= 10',
-	] })
-]}
+module.exports = {
+	plugins: [
+		require('autoprefixer')({
+			browsers: [
+				'last 4 versions',
+				'> 5%',
+				'ie 11',
+				'not ie <= 10',
+			],
+		})
+	],
+}
