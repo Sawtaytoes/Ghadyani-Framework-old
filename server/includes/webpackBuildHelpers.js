e0bb2acd65af47b75575e46b213fba6c416995d0
Kevin Ghadyani <Saturn2888@gmail.com>
2017-10-05 23:27:35 -0500

    Minor refactor in webpackBuildHelpers

--- server/includes/webpackBuildHelpers.js
+++ server/includes/webpackBuildHelpers.js
@@ -1,6 +1,15 @@
 const onBuild = (taskName, serverUrl = '') => (err, stats) => {
-	if (err) { throw console.error('webpack', err) }
-	console.info(`[${taskName}]`, serverUrl, stats ? stats.toString({ colors: true }) : '')
+	if (err) {
+		throw console.error('webpack', err)
+	}
+
+	console.info(
+		`[${taskName}]`,
+		serverUrl,
+		stats
+		? stats.toString({ colors: true })
+		: ''
+	)
 }
 
 module.exports = Object.freeze({
