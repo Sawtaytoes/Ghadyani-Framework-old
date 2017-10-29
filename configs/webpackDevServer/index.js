7e06d741fd590b6e2c0ec731fe63f75a63e82ffd
Kevin Ghadyani <Saturn2888@gmail.com>
2017-10-29 15:38:24 -0500

    Cleaned up webpackDevServer config

--- configs/webpackDevServer/index.js
+++ configs/webpackDevServer/index.js
@@ -2,22 +2,18 @@ const fs = require('fs')
 
 const dir = require(`${global.baseDir}directories`)
 const config = require(`${dir.configs}`)
-const paths = require(`${dir.includes}paths`)
 const webpackClientConfig = require(`${dir.configs}webpack/clientDev`)
 
 module.exports = {
-	contentBase: `${global.baseDir}${paths.root.dest}`,
 	historyApiFallback: true,
 	hot: true,
 	https: config.isSecure() && {
 		cert: config.isSecure() ? fs.readFileSync('./cert/domain-crt.txt') : null,
 		key: config.isSecure() ? fs.readFileSync('./cert/key.pem') : null,
 	},
-	// progress: true,
 	proxy: {
 		'*': { target: config.getProxyServerUrl() }
 	},
 	publicPath: webpackClientConfig.output.publicPath,
-	// quiet: true,
 	stats: 'minimal',
 }
