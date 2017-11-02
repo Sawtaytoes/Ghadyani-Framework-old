783b24ac0c1562fd78b3a48d4440beab232805c2
Kevin Ghadyani <Saturn2888@gmail.com>
2017-11-01 22:39:07 -0500

    Updated Cypress testing test clicker.js

--- cypress/integration/clicker.js
+++ cypress/integration/clicker.js
@@ -1,19 +1,12 @@
-describe('Kitchen Sink', () => {
-	it('.should() - assert that <title> is correct', function(){
-		cy.visit('/')
-		cy.title().should('include', 'Kitchen Sink')
-	})
-})
-
 describe('Home', () => {
 	beforeEach(() => cy.visit('/'))
 
 	it('Has the correct <title>', () => {
-		cy.title().should.be('Home')
+		cy.title().should.be.equal('Home')
 	})
 
 	it('Has the correct page title', () => {
-		cy.get('h1').should.be('Hello World')
+		cy.get('h1').should.be.equal('Hello World')
 	})
 
 	it('Has a clickable link', () => {
@@ -21,18 +14,18 @@ describe('Home', () => {
 	})
 })
 
-// describe('About', () => {
-// 	beforeEach(() => cy.visit('/about'))
+describe('About', () => {
+	beforeEach(() => cy.visit('/about'))
 
-// 	it('Has the correct <title>', () => {
-// 		cy.title().should.be('About')
-// 	})
+	it('Has the correct <title>', () => {
+		cy.title().should.be.equal('About')
+	})
 
-// 	it('Has the correct page title', () => {
-// 		cy.get('h1').should.be('About')
-// 	})
+	it('Has the correct page title', () => {
+		cy.get('h1').should.be.equal('About')
+	})
 
-// 	it('Has a clickable link', () => {
-// 		cy.get('[href=/').should.exist()
-// 	})
-// })
+	it('Has a clickable link', () => {
+		cy.get('[href=/').should.exist()
+	})
+})
